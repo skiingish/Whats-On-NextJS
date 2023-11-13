5b27809d11d0e02d3a9d1c5142861dbf53ee56ba
Sean <96028788+s-corcoran@users.noreply.github.com>
2023-11-13 11:51:20 -0700

    added the pwa stuff

--- next.config.js
+++ next.config.js
@@ -1,8 +1,38 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    serverActions: true,
-  },
-}
 
-module.exports = nextConfig
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disableDevLogs: true,
+});
+
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+const nextConfig = withBundleAnalyzer(
+  withPWA({
+    experimental: {
+      serverActions: true,
+    },
+  })
+);
+
+// const nextConfig = {
+//   experimental: {
+//     serverActions: true,
+//   },
+// };
+
+// const withPWA = require('next-pwa')({
+//   dest: 'public',
+//   register: true,
+//   skipWaiting: true,
+// });
+
+// module.exports = withPWA({
+//   reactStrictMode: true,
+// });
+
+module.exports = nextConfig;
