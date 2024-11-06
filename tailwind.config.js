11c0de252518508da6d3b9b4d3fee528e15c053f
Sean <52271972+skiingish@users.noreply.github.com>
2024-11-06 18:15:14 +1100

    - removed class dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    darkMode: ['class'],
-    content: {
+  content: {
     relative: true,
     files: [
       './app/**/*.{js,ts,jsx,tsx,mdx}',
@@ -11,67 +10,68 @@ module.exports = {
     ],
   },
   theme: {
-  	extend: {
-  		colors: {
-  			background: 'hsl(var(--background))',
-  			'background-secondary': 'hsl(var(--background-secondary))',
-  			'dark-background': 'hsl(var(--dark-background))',
-  			foreground: 'hsl(var(--foreground))',
-  			'dark-foreground': 'hsl(var(--dark-foreground))',
-  			'text-foreground': 'hsl(var(--text-foreground))',
-  			'dark-text-foreground': 'hsl(var(--dark-text-foreground))',
-  			highlight: 'hsl(var(--highlight))',
-  			btn: {
-  				background: 'hsl(var(--btn-background))',
-  				'background-hover': 'hsl(var(--btn-background-hover))',
-  				'secondary-background': 'hsl(var(--btn-secondary-background))',
-  				'secondary-background-hover': 'hsl(var(--btn-secondary-background-hover))'
-  			},
-  			card: {
-  				DEFAULT: 'hsl(var(--card))',
-  				foreground: 'hsl(var(--card-foreground))'
-  			},
-  			popover: {
-  				DEFAULT: 'hsl(var(--popover))',
-  				foreground: 'hsl(var(--popover-foreground))'
-  			},
-  			primary: {
-  				DEFAULT: 'hsl(var(--primary))',
-  				foreground: 'hsl(var(--primary-foreground))'
-  			},
-  			secondary: {
-  				DEFAULT: 'hsl(var(--secondary))',
-  				foreground: 'hsl(var(--secondary-foreground))'
-  			},
-  			muted: {
-  				DEFAULT: 'hsl(var(--muted))',
-  				foreground: 'hsl(var(--muted-foreground))'
-  			},
-  			accent: {
-  				DEFAULT: 'hsl(var(--accent))',
-  				foreground: 'hsl(var(--accent-foreground))'
-  			},
-  			destructive: {
-  				DEFAULT: 'hsl(var(--destructive))',
-  				foreground: 'hsl(var(--destructive-foreground))'
-  			},
-  			border: 'hsl(var(--border))',
-  			input: 'hsl(var(--input))',
-  			ring: 'hsl(var(--ring))',
-  			chart: {
-  				'1': 'hsl(var(--chart-1))',
-  				'2': 'hsl(var(--chart-2))',
-  				'3': 'hsl(var(--chart-3))',
-  				'4': 'hsl(var(--chart-4))',
-  				'5': 'hsl(var(--chart-5))'
-  			}
-  		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		}
-  	}
+    extend: {
+      colors: {
+        background: 'hsl(var(--background))',
+        'background-secondary': 'hsl(var(--background-secondary))',
+        'dark-background': 'hsl(var(--dark-background))',
+        foreground: 'hsl(var(--foreground))',
+        'dark-foreground': 'hsl(var(--dark-foreground))',
+        'text-foreground': 'hsl(var(--text-foreground))',
+        'dark-text-foreground': 'hsl(var(--dark-text-foreground))',
+        highlight: 'hsl(var(--highlight))',
+        btn: {
+          background: 'hsl(var(--btn-background))',
+          'background-hover': 'hsl(var(--btn-background-hover))',
+          'secondary-background': 'hsl(var(--btn-secondary-background))',
+          'secondary-background-hover':
+            'hsl(var(--btn-secondary-background-hover))',
+        },
+        card: {
+          DEFAULT: 'hsl(var(--card))',
+          foreground: 'hsl(var(--card-foreground))',
+        },
+        popover: {
+          DEFAULT: 'hsl(var(--popover))',
+          foreground: 'hsl(var(--popover-foreground))',
+        },
+        primary: {
+          DEFAULT: 'hsl(var(--primary))',
+          foreground: 'hsl(var(--primary-foreground))',
+        },
+        secondary: {
+          DEFAULT: 'hsl(var(--secondary))',
+          foreground: 'hsl(var(--secondary-foreground))',
+        },
+        muted: {
+          DEFAULT: 'hsl(var(--muted))',
+          foreground: 'hsl(var(--muted-foreground))',
+        },
+        accent: {
+          DEFAULT: 'hsl(var(--accent))',
+          foreground: 'hsl(var(--accent-foreground))',
+        },
+        destructive: {
+          DEFAULT: 'hsl(var(--destructive))',
+          foreground: 'hsl(var(--destructive-foreground))',
+        },
+        border: 'hsl(var(--border))',
+        input: 'hsl(var(--input))',
+        ring: 'hsl(var(--ring))',
+        chart: {
+          1: 'hsl(var(--chart-1))',
+          2: 'hsl(var(--chart-2))',
+          3: 'hsl(var(--chart-3))',
+          4: 'hsl(var(--chart-4))',
+          5: 'hsl(var(--chart-5))',
+        },
+      },
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)',
+      },
+    },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [require('tailwindcss-animate')],
 };
